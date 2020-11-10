ed6fcc6634a1a8429c1e797d09a17b8b8aee94bc
Patrick Wang <patrick.wang@isep.fr>
2020-11-10 17:29:41 +0100

    Basic JLabel implementation

--- src/fr/patrickwang/Main.java
+++ src/fr/patrickwang/Main.java
@@ -11,44 +11,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Ceci est le titre de ma fenêtre");
-        frame.setSize(new Dimension(1024, 768));
 
-        JPanel leftPanel = new JPanel();
-        leftPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        leftPanel.setPreferredSize(new Dimension(500, 750));
-        leftPanel.add(new JLabel("<html>Panneau de gauche, <br/>peut-être pour les cartes Alibis?</html>"));
-        frame.add(leftPanel, BorderLayout.LINE_START);
+        JButton jLabel = new JButton("Texte à afficher");
+        jLabel.setIcon(new ImageIcon("0.png"));
+        jLabel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        jLabel.setPreferredSize(new Dimension(500, 500));
+        jLabel.setVerticalTextPosition(SwingConstants.TOP);
+        jLabel.setVerticalAlignment(SwingConstants.CENTER);
+        frame.add(jLabel);
 
-        JPanel centerPanel = new JPanel();
-        centerPanel.setBorder(BorderFactory.createLineBorder(Color.RED));
-        centerPanel.setPreferredSize(new Dimension(750, 750));
-        centerPanel.setLayout(new GridLayout(5, 5));
-        for (int i = 0; i < 25; i++) {
-            BufferedImage buttonIcon = ImageIO.read(new File(i % 8 + ".png"));
-            District district = new District(new ImageIcon(buttonIcon));
-            centerPanel.add(district);
-        }
-        frame.add(centerPanel, BorderLayout.CENTER);
-
-        JPanel rightPanel = new JPanel();
-        rightPanel.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-        rightPanel.setPreferredSize(new Dimension(250, 750));
-        rightPanel.add(new JLabel("<html>Panneau de droite, <br/> peut-être pour les " +
-                "jetons de tours?</html>"));
-        frame.add(rightPanel, BorderLayout.LINE_END);
-
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE));
-        bottomPanel.setPreferredSize(new Dimension(1500, 150));
-        bottomPanel.add(new JLabel("Panneau du bas, peut-être pour les actions des jetons et " +
-                "éventuellement les cartes Alibis de Mr Jack."));
-        frame.add(bottomPanel, BorderLayout.PAGE_END);
-
-        frame.setResizable(false);
         frame.pack();
-        // Je centre par rapport au bureau
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
